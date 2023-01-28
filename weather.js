d54be98c81f76245074c54db92fb0b4bfc9267be
Maaz Nonsola <maaznonsola786@gmail.com>
2023-01-28 15:19:25 +0530

    API Update

--- weather.js
+++ weather.js
@@ -1,17 +1,17 @@
 class Weather {
   constructor(city, state) {
-    this.apiKey = '99dfe35fcb7de1ee';
+    this.apiKey = '5607089479b64fcda021388aac2a6354';
     this.city = city;
     this.state = state;
   }
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&${this.state}&units=metric&appid=${this.apiKey}`);
 
     const responseData = await response.json();
 
-    return responseData.current_observation;
+    return responseData;
   }
 
   // Change weather location
